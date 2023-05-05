748135dd19e52db10c321a4a799c968ed8a0c2b5
nimroddanielmaayan <nimrodmayan@gmail.com>
2023-05-06 02:37:46 +0300

    Daily progress update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-backend-practice-1-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
